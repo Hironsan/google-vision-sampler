01c8b7a4ab22c586fb9f989a1f6e86714a26702e
Hironsan <light.tree.1.13@gmail.com>
2018-08-21 12:23:20 +0900

    Update label detection

---
+++ examples/label_detection.py
@@ -0,0 +1,32 @@
+import argparse
+import io
+import os
+
+from google.cloud import vision
+from google.cloud.vision import types
+
+
+def main(image_file):
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # Loads the image into memory
+    with io.open(image_file, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_file', help='The image you\'d like to label.')
+    args = parser.parse_args()
+    main(args.image_file)
